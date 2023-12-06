1902abc4d1edde3b992c04a600b2d4193991346e
aswanth6000 <aswanth682@gmail.com>
2023-12-06 18:27:23 +0530

    user login and signup success

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config(); 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
+const session = require('express-session');
+const app = express();
 app.use(express.json());
 
 
+app.set('trust proxy', true);
+app.use(session({
+    secret : 'secret-key',
+    resave : false,
+    saveUninitialized : true
+}))
 app.use(require('./routes/userRouter'));
-
 const start = async () => {
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
--- auth/src/routes/userRouter.js
+++ auth/src/routes/userRouter.js
@@ -3,42 +3,63 @@ const router = express.Router()
 const {body, validationResult} = require('express-validator')
 const User = require('../model/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+
 
 router.get('/api/users/currentuser', (req, res)=>{
-    res.send('sdfgsd')
+    res.send('iuhiugiiugh')
+    console.log(`HII therererere`);
+})
+router.post('/api/users/signin', async (req, res)=>{
+    const {username , password} = req.body; 
+    // if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD_HASH) {
+    //     req.session.isAdminLoggedIn = true;
+    //     return res.redirect('/admindashboard');
+    // }
+    const user = await User.findOne({username});
+    console.log(user);
+    if(!user || !bcrypt.compareSync(password, user.password)){
+        return res.status(201).json({ success: false, message: "Invalid username or password" });
+    }
+    req.session.user = user; 
+    res.send('user logged in')
     console.log(`HII therererere`);
 })
 router.post('/api/users/signout', (req, res)=>{
     res.send('sdfgsd')
     console.log(`HII therererere`);
 })
-router.post('/api/users/signup/:id', async (req, res)=>{
-    const {username, password, confirmPassword, phone, email} = req.body;
+router.post('/api/users/signup', async (req, res) => {
+    const { username, password, confirmPassword, phone, email } = req.body;
 
-        if (password !== confirmPassword) {
-            return { success: false, message: "Passwords do not match" };
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({
-            username: username,
-            password: hashedPassword,
-            phoneNumber: phone,
-            email: email
-        });
-        try {
-            await user.save();
-            return { success: true, message: "User registered successfully" };
-        } catch (error) {
-            console.error('Error during signup:', error);
-            res.status(500).render('./user/error', { errorMessage: 'An error occoured during signup', statusCode : '404' });
-        }
-    
-    console.log('creating a user...');
-    res.send({});
-})
-router.post('/api/users/signin', (req, res)=>{
-    res.send('sdfgsd')
-    console.log(`HII therererere`);
-})
+    if (password !== confirmPassword) {
+        return res.status(400).json({ success: false, message: "Passwords do not match" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+        username: username,
+        password: hashedPassword,
+        phoneNumber: phone,
+        email: email
+    });
+console.log(process.env.JWT_KEY);
+    try {
+        await user.save();
+        const userJWT = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY);
+        
+
+        req.session.user = userJWT;
+        console.log(userJWT);
+
+        res.status(201).json({ success: true, message: "User registered successfully" });
+    } catch (error) {
+        console.error('Error during signup:', error);
+        res.status(500).json({ success: false, message: "An error occurred during signup" });
+    }
+});
 
 module.exports = router
