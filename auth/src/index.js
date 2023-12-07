4f08939cc7128065999c5505a135721075912896
aswanth6000 <aswanth682@gmail.com>
2023-12-07 15:23:06 +0530

    signup created

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config(); 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const session = require('express-session');
 const app = express();
 app.use(express.json());
@@ -13,10 +13,13 @@ app.use(session({
     resave : false,
     saveUninitialized : true
 }))
+
+// mongodb://auth-mongo-srv:27017/auth
+
 app.use(require('./routes/userRouter'));
 const start = async () => {
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        await mongoose.connect('mongodb+srv://gadgeteaseinfo:ZR8c5WPdNkn7dI8r@cluster0.tpymocf.mongodb.net/?retryWrites=true&w=majority');
         console.log("MongoDB connected");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
